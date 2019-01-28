d0bb54257bdbccfd4a0eda42838f3bace640cca9
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 19:53:04 +0800

    Your aim is high and to the right.

--- index.php
+++ index.php
@@ -50,7 +50,8 @@ function process_video($fileinfo)
         $audio_setting = "-c:a copy";
         $same_audio = true;
     }
-    echo 'NEW FILE NAME: ' .     // if 
+    echo 'NEW FILE'
+    // if 
     if (!($same_video || $same_audio || $same_file)) {
         $cmd = "ffmpeg -i $old_file_name $video_setting $audio_setting $new_file_name";
         echo $cmd;
