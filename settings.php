8863aeb87a9deb7a03529e4af1eaede3c600f982
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:32:06 +0800

    You will be reincarnated as a toad; and you will be much happier.

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+namespace chipbug\php_video_reformatter;
+
+// SETTINGS
+// location of your media
+// $files = '/yourmedia/videos
+//$files = '/media/pc/Transcend';
+$files = '/media/dellserver/data/videos/series/temp';
+
+// delete original file after conversion - set to 'true'
+// WARNING - you are responsible for making sure you have enough space available!
+$delete_on_conversion = false;
