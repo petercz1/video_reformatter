2dde91c9f0418afc71aeb24e663c3091b2301f32
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 16:09:42 +0800

    You will be the last person to buy a Chrysler.

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ function process_video($fileinfo)
     print_r($codecs);
     if ($codecs['container']<> 'mp4') {
         echo 'converting for mp4' . PHP_EOL;
-        $new_file_name = $fileinfo->getBasename($codecs['container']) . 'mp4';
+        $new_file_name = $fileinfo->getBasename($codecs['container']) . '.mp4';
     }
     if ($codecs['video']<> 'avc') {
         echo 'converting for avc' . PHP_EOL;
