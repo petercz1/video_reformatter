6ed2a0c5d8176ef1d3c99a3b5fe97d4df2be4704
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:41:22 +0800

    So this is it.  We're going to die.

--- settings.php
+++ settings.php
@@ -9,7 +9,7 @@ namespace chipbug\php_video_reformatter;
 $files = '/media/dellserver/data/videos/series/temp';
 
 // filetypes to scan for
-$file_types = ['']
+$file_types = ['webm', 'mp4']
 
 // delete original file after conversion - set to 'true'
 // WARNING - you are responsible for making sure you have enough space available!
