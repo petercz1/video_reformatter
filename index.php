6d1c7b12842a024894a05cb2196de1ef077cfeaa
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 14:09:59 +0800

    Today is what happened to yesterday.

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ function process_video($fileinfo)
 	$results = simplexml_load_string($results);
 	$results->registerXPathNamespace('ns', 'https://mediaarea.net/mediainfo');
     $audio = $results->xpath("//ns:track[@type='Audio']/ns:Format");
-    print_r($audio[0][0]);
+    echo ($audio[0][0]);
 }
 
 get_files($files);
