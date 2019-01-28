31a944e5f6b284bbba9517e9c09baff780b463ce
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:42:23 +0800

    Q:	How many lawyers does it take to change a light bulb? A:	One.  Only it's his light bulb when he's done.

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+namespace chipbug\php_video_reformatter;
+
+require('settings.php');
+
+// uncomment this if you want to see debugging in ./debug.log
+require('debug.php');
+
+function get_files($files)
+{
+    global $filetypes;
+    foreach (new \DirectoryIterator($files) as $fileinfo) {
+        // skip dot files
+        if ($fileinfo->isDot()) {
+            continue;
+        }
+        // recursion
+        if ($fileinfo->isDir()) {
+            get_files($fileinfo->getPathname());
+        }
+        $ext = strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+        // select video containers to process: mkv/mp4/avi/webm
+        if (in_array())
+        switch ($ext) {
+            case 'mkv':
+            case 'mp4':
+            case 'avi':
+            case 'webm':
+            process_video($fileinfo);
+            break;
+        }
+    }
+}
+
+function process_video($fileinfo)
+{
+    $codecs = get_codecs($fileinfo);
+    print_r($codecs);
+
+    # code...
+}
+
+function get_codecs($fileinfo)
+{
+    $video = escapeshellarg($fileinfo->getPathName());
+    //echo $video . PHP_EOL;
+    $cmd = 'mediainfo --Output=XML ' . $video;
+    $results = shell_exec($cmd);
+    $results = simplexml_load_string($results);
+    $ns = $results->getNamespaces();
+    $results->registerXPathNamespace('ns', $ns['']);
+    $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
+    $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
+    $codecs['filename'] = $fileinfo->getFilename();
+    $codecs['container'] =  strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+    $codecs['video'] =  ($video_codec[0][0]->__toString());
+    $codecs['audio'] = ($audio_codec[0][0]->__toString());
+    return $codecs;
+}
+
+get_files($files);
