48dd23994177566e0ea1d9390018d8b0bc545a31
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 19:03:48 +0800

    You definitely intend to start living sometime soon.

--- settings.php
+++ settings.php
@@ -13,7 +13,7 @@ $file_types = ['webm', 'mp4', 'avi', 'mkv'];
 
 // delete original file after conversion - set to 'true'
 // WARNING - you are responsible for making sure you have enough space available!
-$delete_on_conversion = false;
+$delete_on_conversion = true;
 
 // use ./debug.log?
 $debug = true;
