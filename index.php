5381185c11417b64c0d56ef55d6478808eb58a47
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 16:31:48 +0800

    Give thought to your reputation.  Consider changing name and moving to a new town.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ function get_files($files)
         $ext = strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
         // select video containers to process: mkv/mp4/avi/webm
         if (in_array($ext, $file_types)) {
-            process_video($fileinfo);
+            //process_video($fileinfo);
         }
     }
 }
