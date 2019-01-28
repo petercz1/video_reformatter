e94ccc1e2acecf4eecb381d6029935da324bdbb6
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:29:45 +0800

    The fashion wears out more apparel than the man. 		-- William Shakespeare, "Much Ado About Nothing"

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+namespace chipbug\php_video_reformatter;
+
+// uncomment this if you want to see debugging in ./debug.log
+require('debug.php');
+
+// settings
+// location of your media
+//$files = '/media/pc/Transcend';
+$files = '/media/dellserver/data/videos/series/temp';
+// delete original file after conversion
+// WARNING - you are responsible for making sure you have enough space available!
+$delete
+
+function get_files($files)
+{
+    global $filetypes;
+    foreach (new \DirectoryIterator($files) as $fileinfo) {
+        // skip dot files
+        if ($fileinfo->isDot()) {
+            continue;
+        }
+        // recursion
+        if ($fileinfo->isDir()) {
+            get_files($fileinfo->getPathname());
+        }
+        $ext = strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+        // select video containers to process: mkv/mp4/avi/webm
+        switch ($ext) {
+            case 'mkv':
+            case 'mp4':
+            case 'avi':
+            case 'webm':
+            process_video($fileinfo);
+            break;
+        }
+    }
+}
+
+function process_video($fileinfo)
+{
+    $codecs = get_codecs($fileinfo);
+    print_r($codecs);
+
+    # code...
+}
+
+
+function get_codecs($fileinfo)
+{
+    $video = escapeshellarg($fileinfo->getPathName());
+    //echo $video . PHP_EOL;
+    $cmd = 'mediainfo --Output=XML ' . $video;
+    $results = shell_exec($cmd);
+    $results = simplexml_load_string($results);
+    $results->registerXPathNamespace('ns', 'https://mediaarea.net/mediainfo');
+    $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
+    $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
+    $codecs['filename'] = $fileinfo->getFilename();  
+    $codecs['container'] =  strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));  
+    $codecs['video'] =  ($video_codec[0][0]->__toString());
+    $codecs['audio'] = ($audio_codec[0][0]->__toString());
+    return $codecs;
+}
+
+get_files($files);
