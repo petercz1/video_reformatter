d3ddb8ec0248fdeb6d03990b8453df3c833bc598
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 16:14:27 +0800

    Lord, what fools these mortals be! 		-- William Shakespeare, "A Midsummer-Night's Dream"

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ function process_video($fileinfo)
     }
     if ($codecs['audio']<> 'aac') {
         echo 'converting for aac' . PHP_EOL;
+        
     }
     //$cmd = "ffmpeg -i $codecs['filename'] $video_setting $audio_setting out.$extension_setting";
 
