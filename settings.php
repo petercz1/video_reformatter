5aaed51daaae9c6a32ff33e9e8ff3740c440e9d5
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:50:32 +0800

    You love peace.

--- settings.php
+++ settings.php
@@ -14,3 +14,4 @@ $file_types = ['webm', 'mp4', 'avi', 'mkv'];
 // delete original file after conversion - set to 'true'
 // WARNING - you are responsible for making sure you have enough space available!
 $delete_on_conversion = false;
+
