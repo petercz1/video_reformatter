3694d4c4bdf419d9cc393d1fcd1f9aeb2b016aee
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:39:21 +0800

    Accent on helpful side of your nature.  Drain the moat.

--- index.php
+++ index.php
@@ -39,7 +39,6 @@ function process_video($fileinfo)
     # code...
 }
 
-
 function get_codecs($fileinfo)
 {
     $video = escapeshellarg($fileinfo->getPathName());
@@ -48,7 +47,6 @@ function get_codecs($fileinfo)
     $results = shell_exec($cmd);
     $results = simplexml_load_string($results);
     $ns = $results->getNamespaces();
-   echo $ns[''];
     $results->registerXPathNamespace('ns', $ns['']);
     $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
     $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
