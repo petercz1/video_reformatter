c05c8ccb0ce1609d80072a04b7f60d3977d7db6a
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 18:35:28 +0800

    No group of professionals meets except to conspire against the public at large. 		-- Mark Twain

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ function process_video($fileinfo)
         $same_audio = true;
     }
     echo $cmd;
-    if (!($same_video && $same_audio && $same_file)) {
+    if (!($same_video || $same_audio && $same_file)) {
         $cmd = "ffmpeg -i $old_file_name $video_setting $audio_setting $new_file_name";
         $results = shell_exec($cmd);
     }
