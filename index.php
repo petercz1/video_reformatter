bb167ecdc27ebd00c09c16c5d9eb33646a4242be
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:56:54 +0800

    You will be misunderstood by everyone.

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+namespace chipbug\php_video_reformatter;
+
+require('settings.php');
+
+if ($debug) require('debug.php');
+
+function get_files($files)
+{
+    global $file_types;
+    foreach (new \DirectoryIterator($files) as $fileinfo) {
+        // skip dot files
+        if ($fileinfo->isDot()) {
+            continue;
+        }
+        // recursion
+        if ($fileinfo->isDir()) {
+            get_files($fileinfo->getPathname());
+        }
+        $ext = strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+        // select video containers to process: mkv/mp4/avi/webm
+        if (in_array($ext, $file_types)){
+            process_video($fileinfo);
+        }
+    }
+}
+
+function process_video($fileinfo)
+{
+    $codecs = get_codecs($fileinfo);
+    print_r($codecs);
+    if($codecs['video']<> 'AVC'){
+        echo 'converting for AVC';
+    }
+    # code...
+}
+
+function get_codecs($fileinfo)
+{
+    $video = escapeshellarg($fileinfo->getPathName());
+    //echo $video . PHP_EOL;
+    $cmd = 'mediainfo --Output=XML ' . $video;
+    $results = shell_exec($cmd);
+    $results = simplexml_load_string($results);
+    $ns = $results->getNamespaces();
+    $results->registerXPathNamespace('ns', $ns['']);
+    $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
+    $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
+    $codecs['filename'] = $fileinfo->getFilename();
+    $codecs['container'] =  strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+    $codecs['video'] =  ($video_codec[0][0]->__toString());
+    $codecs['audio'] = ($audio_codec[0][0]->__toString());
+    return $codecs;
+}
+
+get_files($files);
