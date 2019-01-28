ea0eccbf00e06af475bc26ce8970c4166188a07c
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 15:36:09 +0800

    Repartee is something we think of twenty-four hours too late. 		-- Mark Twain

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ function get_codecs($fileinfo)
     $results = shell_exec($cmd);
     $results = simplexml_load_string($results);
     $ns = $results->getNamespaces()[0];
-    
+    print_r $ns;
     $results->registerXPathNamespace('ns', 'https://mediaarea.net/mediainfo');
     $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
     $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
