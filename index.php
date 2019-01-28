0ba14cdbfbcb451705ceb5410d1f22c22a226c6f
petercz1 <email.peter.carroll@gmail.com>
2019-01-28 16:13:42 +0800

    You enjoy the company of other people.

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ function process_video($fileinfo)
         echo 'converting for avc' . PHP_EOL;
         $video_setting = "-c:v libx264";
     }else{
-        $video_setting = "-c"
+        $video_setting = "-c:v copy";
     }
     if ($codecs['audio']<> 'aac') {
         echo 'converting for aac' . PHP_EOL;
