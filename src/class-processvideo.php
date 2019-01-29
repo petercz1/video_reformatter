ad0ba6c64f5b2cd2ba27984edd8fed9230ef3b49
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 14:41:02 +0800

    Don't get to bragging.

--- src/class-processvideo.php
+++ src/class-processvideo.php
@@ -3,5 +3,49 @@ namespace chipbug\php_video_reformatter;
 
 class ProcessVideo{
 
+	function process_video($fileinfo)
+{
+    global $delete_on_conversion;
+    echo 'process_video';
+    $same_file = false;
+    $same_video = false;
+    $same_audio = false;
+    $codecs = get_codecs($fileinfo);
+    $old_file_name = escapeshellarg($fileinfo->getPathname());
+    echo PHP_EOL . 'PROCESSING ' . $old_file_name . PHP_EOL;
+    if ($codecs['container']<> 'mp4') {
+        echo 'file not mp4...';
+        $new_file_name = escapeshellarg($fileinfo->getPath() . '/'. $fileinfo->getBasename($codecs['container']) . 'mp4');
+    } else {
+        $new_file_name = escapeshellarg($fileinfo->getPath(). 'new.mp4');
+        $same_file = true;
+    }
+    if ($codecs['video']<> 'avc') {
+        echo 'video not avc...';
+        $video_setting = "-c:v libx264";
+    } else {
+        $video_setting = "-c:v copy";
+        $same_video = true;
+    }
+    if ($codecs['audio']<> 'aac') {
+        echo 'audio not aac...';
+        $audio_setting = "-c:a aac";
+    } else {
+        $audio_setting = "-c:a copy";
+        $same_audio = true;
+    }
+    echo PHP_EOL . 'NEW FILE NAME: ' . $new_file_name;
+    if (!($same_video && $same_audio && $same_file)) {
+        $cmd = "ffmpeg -i $old_file_name $video_setting $audio_setting $new_file_name";
+        echo $cmd;
+        $results = shell_exec($cmd);
+        //print_r($results);
+    }
+    if ( (!$same_file || !$same_video || !$same_audio) && $delete_on_conversion) {
+        echo 'deleting: ' . escapeshellarg($fileinfo->getPathname());
+        unlink($fileinfo->getPathname());
+    }
+}
+
 
 }
