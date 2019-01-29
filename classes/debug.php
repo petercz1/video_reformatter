1fe4368228421071b03900f4074580a485d90d76
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 17:07:53 +0800

    Writing is easy; all you do is sit staring at the blank sheet of paper until drops of blood form on your forehead. 		-- Gene Fowler

--- classes/debug.php
+++ classes/debug.php
@@ -3,7 +3,6 @@ namespace chipbug\php_video_reformatter;
 
 class Debug{
 	public function init(){
-        echo 'doing debug stuff';
         error_reporting(E_ALL);
         ini_set('log_errors', 1);
         ini_set('error_log', "./debug.log");
