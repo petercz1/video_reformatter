a78e89446fa05cc604ce810e1b01aece06460fbf
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 16:49:41 +0800

    Someone is speaking well of you.

--- classes/debug.php
+++ classes/debug.php
@@ -2,7 +2,7 @@
 namespace chipbug\php_video_reformatter;
 
 class Debug{
-	public function __construct(){
+	public function init(){
         error_reporting(E_ALL);
         ini_set('log_errors', 1);
         ini_set('error_log', "./debug.log");
