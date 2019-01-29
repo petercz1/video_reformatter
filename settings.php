d2cc867fd49b9f23005dcb8f1de5ebe8df4a7962
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 13:27:03 +0800

    You will receive a legacy which will place you above want.

--- settings.php
+++ settings.php
@@ -11,7 +11,7 @@ class Settings{
 public $files = '/media/dellserver/data/videos/series/temp';
 
 // filetypes to scan for
-$file_types = ['webm', 'mp4', 'avi', 'mkv'];
+public $file_types = ['webm', 'mp4', 'avi', 'mkv'];
 
 // delete original file after conversion - set to 'true'
 // WARNING - you are responsible for making sure you have enough space available!
