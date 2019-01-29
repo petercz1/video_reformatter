22441a336ddbd74b427a711c5cb4f30bb548289e
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 14:21:26 +0800

    No group of professionals meets except to conspire against the public at large. 		-- Mark Twain

--- src/class-settings.php
+++ src/class-settings.php
@@ -8,21 +8,21 @@ class Settings
     // $files = '/yourmedia/videos';
 
     //$files = '/media/pc/Transcend';
-    public $files = '/media/dellserver/data/videos/series/temp';
+    private $files = '/media/dellserver/data/videos/series/temp';
 
     // filetypes to scan for. Add your own if ffmpeg supports them
     // though to be honest you'd struggle to find a format that isn't supported...
     // https://en.wikipedia.org/wiki/FFmpeg#Supported_codecs_and_formats
-    public $file_types = ['webm', 'mp4', 'avi', 'mkv'];
+    private $file_types = ['webm', 'mp4', 'avi', 'mkv'];
 
     // delete original file after conversion - set to 'true'
     // WARNING - you are responsible for making sure you have enough space available!
-    public $delete_on_conversion = true;
+    private $delete_on_conversion = true;
 
     // use ./debug.log?
-    public $debug = true;
+    private $debug = true;
 
     function init(){
-        
+
     }
 }
