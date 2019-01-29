1d1b0af76c7c8cd072e55a1284acd1c0798d9961
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 16:09:41 +0800

    The human race is a race of cowards; and I am not only marching in that procession but carrying a banner. 		-- Mark Twain

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 namespace chipbug\php_video_reformatter;
 
 spl_autoload_register(function($class){
-    include 'src/' . $class . '.php';
+    include 'src/class-' . $class . '.php';
 });
 
 $settings = (new Settings)->init();
