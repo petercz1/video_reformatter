ddb6122d3cac6852a6a1feacdea2ddc08cf90d16
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 16:01:42 +0800

    Your temporary financial embarrassment will be relieved in a surprising manner.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ spl_autoload_register(function($class){
     include 'src/' . $class . '.php';
 });
 
-$settings = new Settings()()
+$settings = (new Settings()()
 //require('settings.php');
 
 if ($debug) {
