502a9e10f52abf6b658a7a689df39df1f2eb6467
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 13:27:10 +0800

    Think twice before speaking, but don't say "think think click click".

--- settings.php
+++ settings.php
@@ -15,9 +15,9 @@ public $file_types = ['webm', 'mp4', 'avi', 'mkv'];
 
 // delete original file after conversion - set to 'true'
 // WARNING - you are responsible for making sure you have enough space available!
-$delete_on_conversion = true;
+public delete_on_conversion = true;
 
 // use ./debug.log?
-$debug = true;
+public $debug = true;
 
 }
