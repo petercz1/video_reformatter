7f0ab3a01a4b7e30826f9c8c43a8737e4a6d41ba
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 13:27:24 +0800

    Q:	What is the difference between a duck? A:	One leg is both the same.

--- settings.php
+++ settings.php
@@ -1,23 +1,22 @@
 <?php
 namespace chipbug\php_video_reformatter;
 
+class Settings
+{
+    // SETTINGS
+    // location of your media
+    // $files = '/yourmedia/videos';
 
-class Settings{
-// SETTINGS
-// location of your media
-// $files = '/yourmedia/videos';
+    //$files = '/media/pc/Transcend';
+    public $files = '/media/dellserver/data/videos/series/temp';
 
-//$files = '/media/pc/Transcend';
-public $files = '/media/dellserver/data/videos/series/temp';
+    // filetypes to scan for
+    public $file_types = ['webm', 'mp4', 'avi', 'mkv'];
 
-// filetypes to scan for
-public $file_types = ['webm', 'mp4', 'avi', 'mkv'];
+    // delete original file after conversion - set to 'true'
+    // WARNING - you are responsible for making sure you have enough space available!
+    public $delete_on_conversion = true;
 
-// delete original file after conversion - set to 'true'
-// WARNING - you are responsible for making sure you have enough space available!
-public $delete_on_conversion = true;
-
-// use ./debug.log?
-public $debug = true;
-
+    // use ./debug.log?
+    public $debug = true;
 }
