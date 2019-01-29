0984abad952738ac7260e0425174d4d1e78b3c1b
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 16:00:14 +0800

    You would if you could but you can't so you won't.

---
+++ src/class-getcodecs.php
@@ -0,0 +1,23 @@
+<?php
+namespace chipbug\php_video_reformatter;
+
+class GetCodecs
+{
+    public function get_codecs($fileinfo)
+    {
+        $video = escapeshellarg($fileinfo->getPathName());
+        //echo $video . PHP_EOL;
+        $cmd = 'mediainfo --Output=XML ' . $video;
+        $results = shell_exec($cmd);
+        $results = simplexml_load_string($results);
+        $ns = $results->getNamespaces();
+        $results->registerXPathNamespace('ns', $ns['']);
+        $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
+        $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
+        $codecs['filename'] = $fileinfo->getFilename();
+        $codecs['container'] =  strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+        $codecs['video'] =  strtolower($video_codec[0][0]->__toString());
+        $codecs['audio'] = strtolower($audio_codec[0][0]->__toString());
+        return $codecs;
+    }
+}
