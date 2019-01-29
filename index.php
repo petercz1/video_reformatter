6d1b846786abf7262a8cf0fb2151a2d14741d1b3
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 16:24:47 +0800

    Be cautious in your daily affairs.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 namespace chipbug\php_video_reformatter;
 
 spl_autoload_register(function($class){
-    $class = str_replace("\", DIRECTORY_SEPARATOR, $class);
+    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
     echo 'CLASS: ' . $class . PHP_EOL;
     include '/src/class-' . $class . '.php';
 });
