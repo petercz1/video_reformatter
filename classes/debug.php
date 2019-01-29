dac1f1fad2278e65027541bc869adf34764b99c8
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 17:07:33 +0800

    You don't become a failure until you're satisfied with being one.

--- classes/debug.php
+++ classes/debug.php
@@ -3,6 +3,7 @@ namespace chipbug\php_video_reformatter;
 
 class Debug{
 	public function init(){
+        echo 
         error_reporting(E_ALL);
         ini_set('log_errors', 1);
         ini_set('error_log', "./debug.log");
