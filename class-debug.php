4089990214210e3bf030e0836cdd8857684d4fcc
petercz1 <email.peter.carroll@gmail.com>
2019-01-29 13:31:59 +0800

    Every why hath a wherefore. 		-- William Shakespeare, "A Comedy of Errors"

--- class-debug.php
+++ class-debug.php
@@ -3,8 +3,8 @@ namespace chipbug\php_video_reformatter;
 
 class Debug{
 
-	
-error_reporting(E_ALL);
+
+public error_reporting(E_ALL);
 ini_set('log_errors', 1);
 ini_set('error_log', "./debug.log");
 }
