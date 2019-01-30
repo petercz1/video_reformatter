f6676899adbb6732e7af998157255264499038a2
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 17:02:57 +0800

    You have a strong appeal for members of the opposite sex.

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -17,7 +17,7 @@ class GetFiles
                 }
                 // recursion
                 if ($fileinfo->isDir()) {
-                    get_files($fileinfo->getPathname());
+                    $this->get_files($fileinfo->getPathname());
                 }
                 $ext = strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
                 // select video containers to process: mkv/mp4/avi/webm
