e35600a3400f76796f67e2ecfde81f7fc89dbf07
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 16:56:13 +0800

    You will experience a strong urge to do good; but it will pass.

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -9,6 +9,7 @@ class GetFiles
             echo 'GetFiles->init()' . PHP_EOL;
             $file_types = $settings['file_types'];
             $file_location = $settings['file_location_root'];
+            echo 
             foreach (new \DirectoryIterator($file_location) as $fileinfo) {
                 // skip dot files
                 if ($fileinfo->isDot()) {
