77564539e9a80298a27ddc02000e6e39dc79b70d
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 15:59:55 +0800

    You've been leading a dog's life.  Stay off the furniture.

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -8,6 +8,7 @@ class GetFiles
         try {
             echo 'GetFiles->init()' . PHP_EOL;
             $file_types = $settings['file_types'];
+            $file_
             foreach (new \DirectoryIterator($files) as $fileinfo) {
                 // skip dot files
                 if ($fileinfo->isDot()) {
