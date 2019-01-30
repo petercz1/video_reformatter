31ae79a809b2b282302a6f9387829c38ed05dcfa
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 16:59:15 +0800

    You will gain money by a fattening action.

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -10,6 +10,7 @@ class GetFiles
             $file_types = $settings['file_types'];
             $file_location = $settings['file_location_root'];
             foreach (new \DirectoryIterator($file_location) as $fileinfo) {
+                
                 // skip dot files
                 if ($fileinfo->isDot()) {
                     continue;
