8cb77c9e852be7930b0de732a0364f873167b482
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 17:19:31 +0800

    A banker is a fellow who lends you his umbrella when the sun is shining and wants it back the minute it begins to rain. 		-- Mark Twain

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -8,30 +8,30 @@ class GetFiles
 
     public function init($settings)
     {
-        
-            $this->file_types = $settings['file_types'];
-            $this->file_location = $settings['file_location_root'];
-            
+        $this->file_types = $settings['file_types'];
+        $this->file_location = $settings['file_location_root'];
     }
 
-    private function get_files(){
-        try{foreach (new \DirectoryIterator($this->file_location) as $fileinfo) {
-            // skip dot files
-            if ($fileinfo->isDot()) {
-                continue;
+    private function get_files()
+    {
+        try {
+            foreach (new \DirectoryIterator($this->file_location) as $fileinfo) {
+                // skip dot files
+                if ($fileinfo->isDot()) {
+                    continue;
+                }
+                // recursion
+                if ($fileinfo->isDir()) {
+                    $this->get_files($fileinfo->getPathname());
+                }
+                $ext = strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+                // select video containers to process: mkv/mp4/avi/webm
+                if (in_array($ext, $this->file_types)) {
+                    //process_video($fileinfo);
+                    echo $fileinfo->getFilename() . PHP_EOL;
+                }
             }
-            // recursion
-            if ($fileinfo->isDir()) {
-                $this->get_files($fileinfo->getPathname());
-            }
-            $ext = strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
-            // select video containers to process: mkv/mp4/avi/webm
-            if (in_array($ext, $this->file_types)) {
-                //process_video($fileinfo);
-                echo $fileinfo->getFilename() . PHP_EOL;
-            }
-        
-    } }catch (Exception $ex) {
+        } catch (Exception $ex) {
             echo $ex->getMessage();
             echo $ex->getFile() . ': line ' . $ex->getLine() . PHP_EOL;
         }
