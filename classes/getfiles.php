9bcf60dab69098cf75281bc6fb2911a5573a5685
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 15:57:28 +0800

    Let me put it this way: today is going to be a learning experience.

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -6,7 +6,7 @@ class GetFiles
     public function init($settings)
     {
         try {
-            echo 'get_files';
+            echo 'GetFiles';
             global $file_types;
             foreach (new \DirectoryIterator($files) as $fileinfo) {
                 // skip dot files
