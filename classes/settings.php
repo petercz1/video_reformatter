98b975dc6f97e4b87763d5401cffdef85924a4e4
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 15:54:34 +0800

    Don't you wish you had more energy... or less ambition?

--- classes/settings.php
+++ classes/settings.php
@@ -16,7 +16,7 @@ class Settings
     private $file_types = ['webm', 'mp4', 'avi', 'mkv'];
 
     // delete original file after conversion - set to 'true'
-    // WARNING - if you don't delet etheyou are responsible for making sure you have enough space available!
+    // WARNING - if you don't delete thenyou are responsible for making sure you have enough space available!
     private $delete_on_conversion = true;
 
     // use ./debug.log?
