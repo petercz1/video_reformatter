ac0305300247aacc3110e333cca2d04ff5b0f688
petercz1 <email.peter.carroll@gmail.com>
2019-01-30 17:05:30 +0800

    When one burns one's bridges, what a very nice fire it makes. 		-- Dylan Thomas

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -5,14 +5,14 @@ class GetFiles
 {
     private $file_types;
     private $file_location;
-    
+
     public function init($settings)
     {
         try {
             echo 'GetFiles->init()' . PHP_EOL;
              $this->file_types = $settings['file_types'];
          $this->file_location = $settings['file_location_root'];
-            foreach (new \DirectoryIterator($file_location) as $fileinfo) {
+            foreach (new \DirectoryIterator($this->file_location) as $fileinfo) {
                 echo $fileinfo->getFilename();
                 // skip dot files
                 if ($fileinfo->isDot()) {
