028bb8b80bf27419b8bf307d6469de4230b158bc
petercz1 <email.peter.carroll@gmail.com>
2019-01-31 10:12:47 +0800

    It were not best that we should all think alike; it is difference of opinion that makes horse-races. 		-- Mark Twain, "Pudd'nhead Wilson's Calendar"

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -32,8 +32,8 @@ class GetFiles
                 echo $ext - PHP_EOL;
                 // select video containers to process: mkv/mp4/avi/webm
                 if (in_array($ext, $this->file_types)) {
-                    //process_video($fileinfo);
                     echo 'processing video...';
+                    //process_video($fileinfo);
                     print_r( $fileinfo). PHP_EOL;
                 }
             }
