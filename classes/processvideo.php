1764370afdbc53c94f884656207f9c56ae2c7505
petercz1 <email.peter.carroll@gmail.com>
2019-01-31 16:48:02 +0800

    Tuesday is the Wednesday of the rest of your life.

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -24,7 +24,7 @@ class ProcessVideo
         echo PHP_EOL . 'PROCESSING ' . $old_file_name . PHP_EOL;
 
         if ($this->codecs['container']<> 'mp4') {
-            echo 'file not mp4...';
+            echo 'file not mp4...' . PHP_EOL;
             $new_file_name = escapeshellarg($fileinfo->getPath() . '/'. $fileinfo->getBasename($this->codecs['container']) . 'mp4');
         } else {
             echo 'same file name' . PHP_EOL;
