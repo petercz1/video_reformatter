e0193be4da640b10cc76774552e95da21c54a128
petercz1 <email.peter.carroll@gmail.com>
2019-01-31 11:41:21 +0800

    You have Egyptian flu: you're going to be a mummy.

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -4,7 +4,7 @@ namespace chipbug\php_video_reformatter;
 class ProcessVideo
 {
     private $delete_on_conversion;
-    public function init(\DirectoryIterator $fileinfo)
+    public function process(\DirectoryIterator $fileinfo)
     {
         global $delete_on_conversion;
         echo 'process_video';
