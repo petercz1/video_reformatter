16d95c6ffcf8079e4d185a06475828ccf96c53d8
petercz1 <email.peter.carroll@gmail.com>
2019-01-31 18:56:20 +0800

    F.S. Fitzgerald to Hemingway: 	"Ernest, the rich are different from us." Hemingway: 	"Yes.  They have more money."

--- classes/getfiles.php
+++ classes/getfiles.php
@@ -23,7 +23,7 @@ class GetFiles
                 }
                 echo 'TRASH:' . $fileinfo->getPath();
                 if(strpos($fileinfo->getPath, '.Trash')){
-                    continue;
+                    break;
                 }
                 // recursion
                 if ($fileinfo->isDir()) {
