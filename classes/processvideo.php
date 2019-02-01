ef1d6d23e3cc2448d8eeddcd8450b89093774ff7
petercz1 <email.peter.carroll@gmail.com>
2019-02-01 13:06:56 +0800

    My only love sprung from my only hate! Too early seen unknown, and known too late! 		-- William Shakespeare, "Romeo and Juliet"

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -81,9 +81,9 @@ class ProcessVideo
             $audio_setting = "-c:a copy";
             $same_audio = true;
         }
-        
-        echo PHP_EOL . 'NEW FILE NAME: ' . $new_file_name . PHP_EOL;
+
         if (!($same_video && $same_audio && $same_file && $same_mp41)) {
+            echo PHP_EOL . 'NEW FILE NAME: ' . $new_file_name . PHP_EOL;
             $cmd = "ffmpeg -i $old_file_name $video_setting $audio_setting $general_setting $new_file_name";
             echo $cmd . PHP_EOL;
             $results = shell_exec($cmd);
