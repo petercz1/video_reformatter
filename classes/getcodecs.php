cd96da52574497ae8d492818d746a2d89aadcca6
petercz1 <email.peter.carroll@gmail.com>
2019-02-02 14:33:00 +0800

    Wrinkles should merely indicate where smiles have been. 		-- Mark Twain

--- classes/getcodecs.php
+++ classes/getcodecs.php
@@ -14,37 +14,41 @@ class GetCodecs
      */
     public function init(\DirectoryIterator $fileinfo)
     {
-        try{
-        echo 'getting codecs';
-        $video = escapeshellarg($fileinfo->getPathName());
-        // uses mediainfo which can dump results as xml. Not my choice but there we are...
-        $cmd = 'mediainfo --Output=XML ' . $video;
-        $results = shell_exec($cmd);
-        $results = simplexml_load_string($results);
-        // get first namespace and register it
-        $ns = $results->getNamespaces();
-        $results->registerXPathNamespace('ns', $ns['']);
+        try {
+            echo 'getting codecs';
+            $video = escapeshellarg($fileinfo->getPathName());
+            // uses mediainfo which can dump results as xml. Not my choice but there we are...
+            $cmd = 'mediainfo --Output=XML ' . $video;
+            $results = shell_exec($cmd);
+            $results = simplexml_load_string($results);
+            // get first namespace and register it
+            $ns = $results->getNamespaces();
+            $results->registerXPathNamespace('ns', $ns['']);
         
-        // get codecs
-        // CodecID == mp41, mp42 etc
-        $general_codec = $results->xpath("//ns:track[@type='General']/ns:CodecID");
-        // AAC, mp3 etc
-        $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
-        // h.264 etc
-        $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
+            // get codecs
+            // CodecID == mp41, mp42 etc
+            $general_codec = $results->xpath("//ns:track[@type='General']/ns:CodecID");
+            // AAC, mp3 etc
+            $audio_codec = $results->xpath("//ns:track[@type='Audio']/ns:Format");
+            // h.264 etc
+            $video_codec = $results->xpath("//ns:track[@type='Video']/ns:Format");
 
-        // load up the array
-        $codecs['filename'] = $fileinfo->getFilename();
-        $codecs['container'] =  strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
-        if (isset($general_codec[0][0])) {
-            $codecs['general'] =  strtolower($general_codec[0][0]->__toString());
+            // load up the array
+            $codecs['filename'] = $fileinfo->getFilename();
+            $codecs['container'] =  strtolower(pathinfo($fileinfo->getPathname(), PATHINFO_EXTENSION));
+            if (isset($general_codec[0][0])) {
+                $codecs['general'] =  strtolower($general_codec[0][0]->__toString());
+            }
+            if (isset($video_codec[0][0])) {
+                $codecs['video'] =  strtolower($video_codec[0][0]->__toString());
+            }
+            if (isset($audio_codec[0][0])) {
+                $codecs['audio'] = strtolower($audio_codec[0][0]->__toString());
+            }
+            return $codecs;
+        }catch (Exception $ex) {
+            echo $ex->getMessage() . PHP_EOL;
+            echo $ex->getFile() . ': line ' . $ex->getLine() . PHP_EOL;
         }
-        if (isset($video_codec[0][0])) {
-            $codecs['video'] =  strtolower($video_codec[0][0]->__toString());
-        }
-        if (isset($audio_codec[0][0])) {
-            $codecs['audio'] = strtolower($audio_codec[0][0]->__toString());
-        }
-        return $codecs;
     }
 }
