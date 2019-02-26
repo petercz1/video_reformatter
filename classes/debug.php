b6bfd52ab4b170629128dd32973fc92eb78a466a
petercz1 <email.peter.carroll@gmail.com>
2019-02-26 10:20:54 +0800

    You have a strong appeal for members of your own sex.

---
+++ classes/debug.php
@@ -0,0 +1,19 @@
+<?php 
+namespace chipbug\php_video_reformatter;
+
+/**
+ * simple debug object
+ */
+class Debug{
+    /**
+     * create a debug object
+     *
+     * @return void
+     */
+	public function init(){
+        error_reporting(E_ALL);
+        ini_set('log_errors', 1);
+        ini_set('error_log', "./debug.log");
+        error_log('logging on');
+    }
+}
