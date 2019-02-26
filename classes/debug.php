2fde913cc675b31b5bf8b385887af0725b1068e6
petercz1 <email.peter.carroll@gmail.com>
2019-02-26 10:20:43 +0800

    After all, all he did was string together a lot of old, well-known quotations. 		-- H. L. Mencken, on Shakespeare

--- classes/debug.php
+++ classes/debug.php
@@ -11,6 +11,7 @@ class Debug{
      * @return void
      */
 	public function init(){
+        error_log('');
         error_reporting(E_ALL);
         ini_set('log_errors', 1);
         ini_set('error_log', "./debug.log");
