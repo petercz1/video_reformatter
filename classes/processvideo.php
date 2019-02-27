e648fc069b40d806d5d578cc5ba8c1de63d9a179
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 12:51:08 +0800

    Keep it short for pithy sake.

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -48,7 +48,7 @@ class ProcessVideo
             if ($this->codecs['container'] == 'mp4' && $this->codecs['general']<> 'mp41') {
                 echo 'container not mp41 ';
                 $general_setting = "-brand mp41";
-                $new_file_name = $fileinfo->getPath() . '/'. $fileinfo->getBasename() . 'new.mp4');
+                $new_file_name = $fileinfo->getPath() . '/'. $fileinfo->getBasename() . 'new.mp4';
                 echo PHP_EOL . __LINE__ . ', setting new file: ' . $new_file_name . PHP_EOL;
             } else {
                 echo 'same mp41 ';
@@ -59,7 +59,7 @@ class ProcessVideo
             // check if container <> mp4
             if ($this->codecs['container']<> 'mp4') {
                 echo 'file not mp4 ';
-                $new_file_name = $fileinfo->getPath() . '/'. $fileinfo->getBasename($this->codecs['container'] . 'mp4');
+                $new_file_name = $fileinfo->getPath() . '/'. $fileinfo->getBasename(. 'mp4');
                 echo PHP_EOL . __LINE__ . ', setting new file: ' . $new_file_name . PHP_EOL;
             } else {
                 echo 'same file name ';
