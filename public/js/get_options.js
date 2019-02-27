c3e5f5c625a6a34ea9d3a394621c05e9325664a8
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 16:12:49 +0800

    You will be called upon to help a friend in trouble.

--- public/js/get_options.js
+++ public/js/get_options.js
@@ -59,7 +59,7 @@ function build_dropdown(option) {
 
 }
 
-function create_element() {
+function create_div() {
   div = document.createElement("div");
   div.setAttribute("class", "form-group");
   //   // create text label out of key by removing underscores
