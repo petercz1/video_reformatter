b2edaa617b0afba9646f19a3f720a41aafe4ffa6
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 13:24:29 +0800

    You are not dead yet.  But watch for further reports.

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -90,7 +90,7 @@ class ProcessVideo
                 echo PHP_EOL . 'new file: ' . $new_file_name . PHP_EOL;
                 if (!file_exists($new_file_name)) {
                     echo 'doesnt exist' . PHP_EOL;
-                    $old_file_name = \escapeshellarg($old_file_name)
+                    $old_file_name = \escapeshellarg($old_file_name);
                     $new_file_name = \escapeshellarg($new_file_name);
                     $cmd = \escapeshellcmd("ffmpeg -hide_banner -loglevel panic -i $old_file_name $video_setting $audio_setting $general_setting $new_file_name");
                     echo $cmd . PHP_EOL;
