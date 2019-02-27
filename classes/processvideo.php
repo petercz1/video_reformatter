628a89cac938b44a39d9c9acbd1f04b459b904a2
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 11:51:34 +0800

    Keep it short for pithy sake.

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -87,6 +87,7 @@ class ProcessVideo
                 echo PHP_EOL . 'new file: ' . $new_file_name . PHP_EOL;
                 if(!is_file($new_file_name)){
                     echo 'doesnt exist' . PHP_EOL;
+                    var_dump()
                     $cmd = \escapeshellcmd("ffmpeg -hide_banner -loglevel panic -i $old_file_name $video_setting $audio_setting $general_setting $new_file_name");
                     //echo $cmd . PHP_EOL;
                     $results = shell_exec($cmd);
