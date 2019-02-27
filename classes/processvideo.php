75202692070f76bd2806a2dd1113c588e31647c2
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 13:05:17 +0800

    Suspicion always haunts the guilty mind. 		-- Wm. Shakespeare

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -48,7 +48,7 @@ class ProcessVideo
             if ($this->codecs['container'] == 'mp4' && $this->codecs['general']<> 'mp41') {
                 echo 'container not mp41 ';
                 $general_setting = "-brand mp41";
-                $new_file_name = $fileinfo->getPath() . '/' . pathinfo($fileinfo->getPathname(), PATHINFO_FILENAME) . 'new.mp4';
+                $new_file_name = $fileinfo->getPath() . '/' . pathinfo($fileinfo->getPathname(), PATHINFO_FILENAME) . '.new.mp4';
                 // $new_file_name = $fileinfo->getPath() . '/'. $fileinfo->getBasename() . 'new.mp4';
                 echo PHP_EOL . __LINE__ . ', setting new file: ' . $new_file_name . PHP_EOL;
             } else {
@@ -65,7 +65,7 @@ class ProcessVideo
                 echo PHP_EOL . __LINE__ . ', setting new file: ' . $new_file_name . PHP_EOL;
             } else {
                 echo 'same file name ';
-                $new_file_name = $fileinfo->getPath() . '/' . pathinfo($fileinfo->getPathname(), PATHINFO_FILENAME) . 'new.mp4';
+                $new_file_name = $fileinfo->getPath() . '/' . pathinfo($fileinfo->getPathname(), PATHINFO_FILENAME) . '.new.mp4';
                 // $new_file_name = $fileinfo->getPath() . '/'. $fileinfo->getBasename() . 'new.mp4';
                 echo PHP_EOL . __LINE__ . ', setting new file: ' . $new_file_name . PHP_EOL;
                 $same_file = true;
