7dd7f1a387627b5b32297b9608597fcca8395139
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 13:19:49 +0800

    Q:	Why do mountain climbers rope themselves together? A:	To prevent the sensible ones from going home.

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -75,7 +75,7 @@ class ProcessVideo
             if ($this->codecs['container'] == 'mp4') {
                 echo 'container is mp4 ';
                 $new_file_name = $fileinfo->getPath() . '/' . pathinfo($fileinfo->getPathname(), PATHINFO_FILENAME) . '.new.mp4';
-            } elseif ($this->codecs['container']<> 'mp4') {
+            } else ($this->codecs['container']<> 'mp4') {
                 echo 'file not mp4 ';
                 $new_file_name = $fileinfo->getPath() . '/' . pathinfo($fileinfo->getPathname(), PATHINFO_FILENAME) . '.mp4';
             } else {
