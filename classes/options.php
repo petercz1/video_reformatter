1b4464bb7ec66ea7f1b35fdb95f7545154b44ff4
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 11:14:48 +0800

    Best of all is never to have been born.  Second best is to die soon.

---
+++ classes/options.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+namespace chipbug\php_video_reformatter;
+
+/**
+ * builds an array of options to be used in various places
+ */
+class Options
+{
+    private static $options;
+    /**
+     * builds an array of options
+     *
+     * @return array
+     */
+    public function init()
+    {
+    }
+
+    public static function getOptions():array
+    {
+        try {
+            $options = json_decode(\file_get_contents(__DIR__ . '/../data/options.json'), true);
+            return $options;
+        } catch (\Throwable $th) {
+            error_log($th->getFile() . ': line ' . $th->getLine() . ', ' . $th->getMessage());
+        }
+    }
+
+    public function setOptions()
+    {
+        // TODO set options
+    }
+}
