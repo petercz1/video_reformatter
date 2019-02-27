55acb2a82d29663538573c3bc1f5f71154892ea2
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 15:18:47 +0800

    If you tell the truth you don't have to remember anything. 		-- Mark Twain

--- classes/options.php
+++ classes/options.php
@@ -20,7 +20,7 @@ class Options
         try {
             if(!isset(self::$options)){
                 self::$options = json_decode(\file_get_contents(__DIR__ . '/../data/options.json'), true);
-                self::$options = 
+                self::$options = $settings;
             }
             print_r(self::$options);
             return self::$options;
