ac739c52f815a666fe562c07620af623da62fbe4
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 11:35:38 +0800

    Truth is the most valuable thing we have -- so let us economize it. 		-- Mark Twain

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -85,6 +85,7 @@ class ProcessVideo
             // execute ffmpeg
             if (!($same_video && $same_audio && $same_file && $same_mp41)) {
                 echo PHP_EOL . 'new file: ' . $new_file_name . PHP_EOL;
+                if(is_file($new_file_name))
                 $cmd = \escapeshellcmd("ffmpeg -i $old_file_name $video_setting $audio_setting $general_setting $new_file_name");
                 echo $cmd . PHP_EOL;
                 //$results = shell_exec($cmd);
