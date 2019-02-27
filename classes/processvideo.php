6a26294a2c5efa79288760a1443356bfe2541f49
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 12:19:28 +0800

    You're ugly and your mother dresses you funny.

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -87,6 +87,7 @@ class ProcessVideo
                 echo PHP_EOL . 'new file: ' . $new_file_name . PHP_EOL;
                 if(!file_exists($new_file_name)){
                     echo 'doesnt exist' . PHP_EOL;
+                    echo $new
                     echo strcmp("/home/pc/data/downloads/misc/demo/folder 1/filemkvmp3.mp4", $new_file_name) . PHP_EOL;
                     var_dump(!is_file($new_file_name));
                     $cmd = \escapeshellcmd("ffmpeg -hide_banner -loglevel panic -i $old_file_name $video_setting $audio_setting $general_setting $new_file_name");
