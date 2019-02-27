41788bea858abfe622a4204081eaa0d3a47dbb8c
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 13:32:55 +0800

    In the first place, God made idiots; this was for practice; then he made school boards. 		-- Mark Twain

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -99,7 +99,7 @@ class ProcessVideo
             // delete file if necessary
             if ((!$same_file || !$same_video || !$same_audio || !$same_mp41) && $this->options['delete_on_conversion']) {
                 echo 'deleting: ' . $old_file_name . PHP_EOL;
-                unlink($fileinfo->getPathname());
+                //unlink($fileinfo->getPathname());
             }
         } catch (\Throwable $th) {
             error_log($th->getFile() . ': line ' . $th->getLine() . ', ' . $th->getMessage());
