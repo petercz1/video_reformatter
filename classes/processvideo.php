4cda1aff5c307e80795c91b749ac6ec551841d3c
petercz1 <email.peter.carroll@gmail.com>
2019-02-27 15:40:40 +0800

    Writing is easy; all you do is sit staring at the blank sheet of paper until drops of blood form on your forehead. 		-- Gene Fowler

--- classes/processvideo.php
+++ classes/processvideo.php
@@ -86,7 +86,7 @@ class ProcessVideo
             }
 
             // delete file if necessary
-            if ((!$same_file || !$same_video || !$same_audio || !$same_mp41) && $this->options['delete_on_conversion']) {
+            if ((!$same_file || !$same_video || !$same_audio || !$same_mp41) && $this->options['delete_on_conversion']['value']) {
                 //unlink($fileinfo->getPathname());
             }
         } catch (\Throwable $th) {
